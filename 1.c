a083b5e37322043683e806633dda7acb9e633886
rkr01438 <30350343+rkr01438@users.noreply.github.com>
2017-07-21 21:40:35 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int main(void)
+{
+int a;
+  printf("enter the number \n");
+  scanf("%d", &a);
+  if(num>0)
+  {
+    printf("positive number");
+  }
+  else if(num<0)
+  {
+    printf("negative number");
+  }
+  else
+  {
+    printf("entered zero");
+  }
+  return o;
+}
