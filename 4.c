2a907caef4446269a0831745d08f5f78fa54de57
rkr01438 <30350343+rkr01438@users.noreply.github.com>
2017-08-04 16:49:22 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char r;
+    printf("Enter a character: ");
+    scanf("%c",&r);
+ 
+    if( (r>='a' && r<='z') || (r>='A' && r<='Z'))
+        printf("%c is an alphabet.",r);
+    else
+        printf("%c is not an alphabet.",r);
+ 
+    return 0;
+}
