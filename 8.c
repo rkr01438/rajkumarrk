16fd69c21da977860c4122e3e5d595cfc958f248
rkr01438 <30350343+rkr01438@users.noreply.github.com>
2017-08-05 16:33:47 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i, a, s=0;
+    printf("Enter upper limit: ");
+    scanf("%d", &a);
+    for(i=1; i<=n; i++)
+    {
+        s += i;
+    }
+    printf("Sum of first %d natural numbers = %d", a, s); 
+    return 0;
+} 
